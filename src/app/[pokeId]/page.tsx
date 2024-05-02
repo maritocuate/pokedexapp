9226170a0ca84326d986d4ce7bd713e2687d41cf
maritocuate <mario300@gmail.com>
2024-05-02 15:54:34 -0300

    ✨ details skeleton

--- src/app/[pokeId]/page.tsx
+++ src/app/[pokeId]/page.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from 'react'
 import DetailCard from '../components/details/DetailCard'
 import { Container } from '@mui/material'
+import { DetailsSkeleton } from '../ui/skeletons'
 
 interface detailsPageProps {
   params: {
@@ -12,7 +14,9 @@ export default async function Page({ params }: detailsPageProps) {
 
   return (
     <Container maxWidth="md" sx={{ my: 4, minHeight: 'calc(100vh - 14rem)' }}>
-      <DetailCard id={Number(pokeId)} />
+      <Suspense fallback={<DetailsSkeleton />}>
+        <DetailCard id={Number(pokeId)} />
+      </Suspense>
     </Container>
   )
 }
---
+++ src/app/ui/skeletons.tsx
@@ -0,0 +1,5 @@
+import { Skeleton } from '@mui/material'
+
+export function DetailsSkeleton() {
+  return <Skeleton variant="rectangular" height={300}></Skeleton>
+}
