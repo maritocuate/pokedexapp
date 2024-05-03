f2847aa6ca19c9fc3329f6cd462870cb933b99ff
maritocuate <mario300@gmail.com>
2024-05-03 13:13:28 -0300

    🐛 scroll prod

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -7,15 +7,10 @@ interface useInfiniteScrollProps {
 
 const useInfiniteScroll = ({ fetchData, offset }: useInfiniteScrollProps) => {
   useEffect(() => {
-    console.log('1.UE')
     const handleScroll = () => {
-      console.log('2.Scroll')
       const windowHeight = window.innerHeight
-      const scrollTop = document.documentElement.scrollTop
+      const scrollTop = Math.ceil(document.documentElement.scrollTop)
       const offsetHeight = document.documentElement.offsetHeight
-      console.log('windowHeight: ' + windowHeight)
-      console.log('scrollTop: ' + scrollTop)
-      console.log('offsetHeight: ' + offsetHeight)
 
       if (windowHeight + scrollTop !== offsetHeight) return
       fetchData(offset)
