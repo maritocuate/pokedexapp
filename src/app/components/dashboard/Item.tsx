e36d3151dc38b1fb87a362eaa14f23aaebf61de4
maritocuate <mario300@gmail.com>
2024-05-04 17:25:51 -0300

    seo refactor

--- src/app/components/dashboard/Item.tsx
+++ src/app/components/dashboard/Item.tsx
@@ -1,4 +1,4 @@
-import { POKEMON_ARTWORK_URL } from '@/app/config/pokemonUrl'
+import { POKEMON_THUMB_URL } from '@/app/config/pokemonUrl'
 import { PokemonResult } from '@/types'
 import { Card, CardContent, CardMedia, Typography } from '@mui/material'
 import Link from 'next/link'
@@ -18,7 +18,7 @@ export default function Item({ data }: ItemProps) {
           alt={name}
           className="hover:scale-105 duration-100 ease-in-out"
           sx={{ height: '14rem', objectFit: 'contain', padding: '3rem' }}
-          image={`${POKEMON_ARTWORK_URL}/${name}.jpg`}
+          image={`${POKEMON_THUMB_URL}/${name}.avif`}
         />
         <CardContent sx={{ borderTop: '1px solid #e5e5e5' }}>
           <Typography variant="h6" className="capitalize">
--- src/app/config/pokemonUrl.ts
+++ src/app/config/pokemonUrl.ts
@@ -1,2 +1,4 @@
+export const POKEMON_THUMB_URL =
+  'https://img.pokemondb.net/sprites/home/normal/2x/avif'
 export const POKEMON_ARTWORK_URL = 'https://img.pokemondb.net/artwork/large'
 export const POKEAPI_URL = 'https://pokeapi.co/api/v2'
