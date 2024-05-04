38af71625b143acf0f71b348c738bedd3d3cd8c6
maritocuate <mario300@gmail.com>
2024-05-04 17:38:44 -0300

    🛠️  seo refactor

--- src/app/components/dashboard/Item.tsx
+++ src/app/components/dashboard/Item.tsx
@@ -1,6 +1,7 @@
 import { POKEMON_THUMB_URL } from '@/app/config/pokemonUrl'
 import { PokemonResult } from '@/types'
 import { Card, CardContent, CardMedia, Typography } from '@mui/material'
+import Image from 'next/image'
 import Link from 'next/link'
 
 interface ItemProps {
@@ -13,12 +14,13 @@ export default function Item({ data }: ItemProps) {
   return (
     <Card>
       <Link href={`/${name}`}>
-        <CardMedia
-          component="img"
+        <Image
           alt={name}
-          className="hover:scale-105 duration-100 ease-in-out"
-          sx={{ height: '14rem', objectFit: 'contain', padding: '3rem' }}
-          image={`${POKEMON_THUMB_URL}/${name}.avif`}
+          className="hover:scale-105 duration-100 ease-in-out object-contain p-10"
+          height={250}
+          width={250}
+          src={`${POKEMON_THUMB_URL}/${name}.avif`}
+          priority
         />
         <CardContent sx={{ borderTop: '1px solid #e5e5e5' }}>
           <Typography variant="h6" className="capitalize">
